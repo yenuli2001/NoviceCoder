ece373d8d8a0696a4a6c68c5418349552ae45abb
IT21304910 <yenuli.sirisena@yahoo.com>
2024-09-22 16:21:42 +0530

    Update profile.jsx

--- client/src/components/profile/profile.jsx
+++ client/src/components/profile/profile.jsx
@@ -7,8 +7,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { updateProfilePicture } from '../../redux/actions/profile';
 import { toast } from 'react-hot-toast';
 import { removeFromPlaylist } from '../../redux/actions/course';
-import Footer from '../layout/Footer'
-import backgroundImage1 from '../../assets/images/img2.jpeg';
+import Footer from '../layout/Footer';
+import backgroundImage3 from '../../assets/images/img5.jpeg';
+import ProgressBar from 'react-progressbar';
 
 const Profile = ({ user }) => {
   const [avatar, setAvatar] = useState(null);
@@ -43,7 +44,6 @@ const Profile = ({ user }) => {
   }, [dispatch, profileError, profileMessage, courseError, courseMessage]);
 
   const removeFromPlaylistHandler = id => {
-    console.log(id);
     dispatch(removeFromPlaylist(id));
   };
 
@@ -59,7 +59,7 @@ const Profile = ({ user }) => {
     }
   };
 
-  const updateAvator = e => {
+  const updateAvatar = e => {
     e.preventDefault();
     if (avatar && avatarPreview !== user.avatar.url) {
       const myForm = new FormData();
@@ -69,138 +69,169 @@ const Profile = ({ user }) => {
   };
 
   return (
-
     <>
-      <div className="bg-gradient-to-r from-blue-500 to-purple-600 items-center">
-        <img src={backgroundImage1} alt="Description of image 2" className="w-full h-48 object-cover" style={{ marginTop: '5px', marginBottom: '20px', paddingLeft: '10px', paddingRight: '10px', height: '300px' }} />
-        <h1 className="mb-6 text-4xl font-bold text-center">Welcome to NoviceCoder!</h1>
-      <div className="p-6 border rounded-lg shadow-lg bg-gradient-to-r from-blue-500 to-purple-600 hover:shadow-xl transition-shadow duration-300">
-        <h1 className="text-3xl font-bold text-center mb-6 text-black-800">
-          Profile
-        </h1>
-        <div className="flex flex-col items-center">
-          {/* Add avatar preview here */}
-          <div className="mb-4">
-            {avatarPreview ? (
-              <img
-                src={avatarPreview}
-                alt="Avatar Preview"
-                className="w-32 h-32 rounded-full object-cover"
-              />
-            ) : user.avatar.url ? (
-              <img
-                src={user.avatar.url}
-                alt="Avatar Preview"
-                className="w-32 h-32 rounded-full object-cover"
-              />
-            ) : (
-              <FaRegUserCircle size={128} className="text-gray-400" />
-            )}
-          </div>
+      <div style={{ backgroundColor: '#845695', minHeight: '100vh', paddingBottom: '40px' }}>
+        {/* Header section with background image */}
+        <div className="relative">
+          <img
+            src={backgroundImage3}
+            alt="Profile Background"
+            className="w-full h-64 object-cover"
+            style={{ filter: 'brightness(80%)' }} // Slightly darken the image
+          />
+        </div>
 
-          <div className="mb-6">
-            <form
-              onSubmit={updateAvator}
-              className="flex flex-col items-center"
-            >
-              <input
-                id="avatar-input"
-                type="file"
-                accept="image/*"
-                onChange={handleAvatarChange}
-                style={{ display: 'none' }}
-              />
-              <label
-                htmlFor="avatar-input"
-                className="cursor-pointer text-blue-800 hover:underline mb-2"
-              >
-                Change Photo
-              </label>
-              <button
-                type="submit"
-                className="px-4 py-2 bg-yellow-500 text-white rounded-lg hover:bg-yellow-600 transition duration-300 transform hover:scale-105"
-              >
-                Submit Photo Changes
+        <div className="flex justify-between items-center" style={{ marginTop: 30, paddingLeft: 20, paddingRight: 20 }}>
+        <h1 className="text-5xl font-bold text-white" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.7)', color: 'black' }}>
+            Welcome,{user.name}!
+          </h1>
+
+          {/* Add Buttons for Request Course and All Courses */}
+          <div className="flex space-x-4">
+            <Link to="/request">
+              <button className="px-4 py-2 bg-indigo-500 text-white rounded-lg hover:bg-indigo-600 transition duration-300">
+                Request a Course
               </button>
-            </form>
+            </Link>
+            <Link to="/courses">
+              <button className="px-4 py-2 bg-indigo-500 text-white rounded-lg hover:bg-indigo-600 transition duration-300">
+                All Courses
+              </button>
+            </Link>
           </div>
         </div>
-        <div className="space-y-4 bg-indigo-200 text-center border border-gray rounded-lg shadow-lg" style={{paddingTop: '20px'}}>
-          <div className="space-y-4 bg-blue-200 text-center border border-gray rounded-lg shadow-lg" style={{margin:'9px'}}>
-            <h3 className="text-lg font-semibold text-black-700">Name</h3>
-            <p className="text-gray-900">{user.name}</p>
-          </div>
-          <div className="space-y-4 bg-blue-200 text-center border border-gray rounded-lg shadow-lg" style={{margin:'9px'}}>
-            <h3 className="text-lg font-semibold text-black-700">Email</h3>
-            <p className="text-gray-900">{user.email}</p>
-          </div>
-          <div className="space-y-4 bg-blue-200 text-center border border-gray rounded-lg shadow-lg" style={{margin:'9px'}}>
-            <h3 className="text-lg font-semibold text-black-700">CreatedAt</h3>
-            <p className="text-gray-900">{user.createdAt.split('T')[0]}</p>
-          </div>
-          {user.role !== 'admin' && (
-            <div>
-              <h3 className="text-lg font-semibold text-black-700">
-                Subscription
-              </h3>
-              {user.subscription && user.subscription.status === 'active' ? (
-                <button className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition duration-300 transform hover:scale-105">
-                  Cancel Subscription
-                </button>
-              ) : (
-                <Link to="/subscribe">
-                  <button className="px-4 py-2 bg-zinc-500 text-white rounded-lg hover:bg-yellow-600 transition duration-300 transform hover:scale-105">
-                    Subscribe
+
+        <div className="w-full max-w-screen-xl mx-auto mt-20 p-8 rounded-xl bg-orange-100">
+          <div className="flex flex-col lg:flex-row gap-8">
+            {/* Profile Section */}
+            <div className="flex-1 bg-gray-800 p-8 rounded-xl shadow-lg">
+              <h2 className="text-2xl font-bold text-white mb-6 text-center">Profile</h2>
+              <div className="flex flex-col items-center">
+                <div className="relative mb-6">
+                  {avatarPreview ? (
+                    <img
+                      src={avatarPreview}
+                      alt="Avatar Preview"
+                      className="w-36 h-36 rounded-full object-cover border-4 border-white shadow-lg"
+                    />
+                  ) : user.avatar.url ? (
+                    <img
+                      src={user.avatar.url}
+                      alt="Avatar"
+                      className="w-36 h-36 rounded-full object-cover border-4 border-white shadow-lg"
+                    />
+                  ) : (
+                    <FaRegUserCircle size={144} className="text-gray-300" />
+                  )}
+                </div>
+
+                <form onSubmit={updateAvatar} className="text-center">
+                  <input
+                    id="avatar-input"
+                    type="file"
+                    accept="image/*"
+                    onChange={handleAvatarChange}
+                    style={{ display: 'none' }}
+                  />
+                  <label
+                    htmlFor="avatar-input"
+                    className="text-indigo-600 cursor-pointer hover:underline mb-6 block"
+                  >
+                    Change Profile Picture
+                  </label>
+                  <button
+                    type="submit"
+                    className="px-4 py-2 bg-indigo-500 text-white rounded-lg hover:bg-indigo-600 transition duration-300"
+                  >
+                    Save Changes
                   </button>
-                </Link>
+                </form>
+              </div>
+
+              <div className="mt-8 text-center">
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 text-left">
+                  <div className="bg-gray-100 p-4 rounded-lg">
+                    <h3 className="text-lg font-semibold text-gray-600">Name</h3>
+                    <p className="text-gray-900">{user.name}</p>
+                  </div>
+                  <div className="bg-gray-100 p-4 rounded-lg">
+                    <h3 className="text-lg font-semibold text-gray-600">Email</h3>
+                    <p className="text-gray-900">{user.email}</p>
+                  </div>
+                  <div className="sm:col-span-2 flex justify-center">
+                    <div className="bg-gray-100 p-4 rounded-lg w-full sm:w-1/2">
+                      <h3 className="text-lg font-semibold text-gray-600 text-center">Joined On</h3>
+                      <p className="text-gray-900 text-center">{user.createdAt.split('T')[0]}</p>
+                    </div>
+                  </div>
+
+                </div>
+
+                <div className="flex justify-center space-x-4 mt-8">
+                  <Link to="/updateprofile">
+                    <button className="px-4 py-2 bg-purple-500 text-white rounded-lg hover:bg-purple-600 transition duration-300">
+                      Update Profile
+                    </button>
+                  </Link>
+                  <Link to="/changepassword">
+                    <button className="px-4 py-2 bg-purple-500 text-white rounded-lg hover:bg-purple-600 transition duration-300">
+                      Change Password
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+
+            {/* Playlist Section */}
+            <div className="flex-1 bg-gray-800 p-6 rounded-xl shadow-lg">
+              <h2 className="text-2xl font-bold text-white mb-6 text-center">Your Playlist</h2>
+              {user.playlist.length > 0 ? (
+                <div className="space-y-6">
+                  {user.playlist.map(item => (
+                    <div key={item.course} className="flex flex-col bg-gray-50 p-6 rounded-lg shadow-lg">
+                      <div className="flex items-center justify-between mb-4">
+                        <img
+                          src={item.poster}
+                          alt="Playlist Image"
+                          className="w-16 h-16 rounded-lg object-cover"
+                        />
+                        <div className="flex-1 ml-4">
+                          <h3 className="text-md font-bold text-gray-700">{item.title}</h3>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                          <Link to={`/course/${item.course}`}>
+                            <button className="px-4 py-2 bg-indigo-500 text-white rounded-lg hover:bg-indigo-600 transition duration-300">
+                              Watch Now
+                            </button>
+                          </Link>
+                          <button
+                            onClick={() => removeFromPlaylistHandler(item.course)}
+                            className="flex items-center px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition duration-300"
+                          >
+                            <RiDeleteBin7Fill className="mr-2" /> Delete
+                          </button>
+                        </div>
+                      </div>
+                      <div className="w-full bg-gray-300 rounded-full h-2.5 mt-4">
+                        <ProgressBar
+                          completed={item.progress}
+                          className="bg-indigo-600 h-2.5 rounded-full"
+                          style={{ width: `${item.progress}%` }}
+                        />
+                      </div>
+                      <p className="text-sm text-gray-500 mt-1">{item.progress}% completed</p>
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                <p className="text-center text-gray-500">Your playlist is empty. Start adding some courses!</p>
               )}
             </div>
-          )}
-          <div className="flex space-x-4 mt-4 justify-center" style={{paddingBottom:'20px'}}>
-            <Link to="/updateprofile">
-              <button className="px-4 py-2 bg-yellow-500 text-white rounded-lg hover:bg-yellow-600 transition duration-300 transform hover:scale-105">
-                Update Profile
-              </button>
-            </Link>
-            <Link to="/changepassword">
-              <button className="px-4 py-2 bg-purple-500 text-white rounded-lg hover:bg-purple-600 transition duration-300 transform hover:scale-105">
-                Change Password
-              </button>
-            </Link>
           </div>
         </div>
       </div>
-      <div className="mt-8 p-6 border rounded-lg shadow-lg bg-gradient-to-r from-blue-500 to-purple-600 hover:shadow-xl transition-shadow duration-300">
-        <h2 className="text-2xl font-bold text-center mb-6 text-black-800">
-          PlayList
-        </h2>
-        {user.playlist.length > 0 && (
-          <div className="space-y-4 bg-indigo-200 text-center border border-gray rounded-lg shadow-lg" style={{paddingTop: '20px'}}>
-            {user.playlist.map(item => (
-              <div key={item.course} className="flex items-center space-x-4 " style={{margin:'20px'}}>
-                <img
-                  src={item.poster}
-                  alt="Playlist Img"
-                  className="w-16 h-16 rounded-lg object-cover"
-                />
-                <Link to={`/course/${item.course}`}>
-                  <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-300 transform hover:scale-105">
-                    Watch Now
-                  </button>
-                </Link>
-                <button
-                  onClick={() => removeFromPlaylistHandler(item.course)}
-                  className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition duration-300 transform hover:scale-105 flex items-center space-x-2 space-between"
-                >
-                  <RiDeleteBin7Fill /> Delete
-                </button>
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-      </div>
-      <Footer/>
+
+      <Footer />
     </>
   );
 };
